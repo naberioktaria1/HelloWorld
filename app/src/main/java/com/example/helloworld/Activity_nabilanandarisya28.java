607adff6de2a8f7580c14813c62f19e123a5025c
nabilanandarisya28 <nabilanandarisyaa28@gmail.com>
2020-09-21 21:45:04 +0700

    New activity

---
+++ app/src/main/java/com/example/helloworld/Activity_nabilanandarisya28.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_nabilanandarisya28 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nabilanandarisya28);
+    }
+}
